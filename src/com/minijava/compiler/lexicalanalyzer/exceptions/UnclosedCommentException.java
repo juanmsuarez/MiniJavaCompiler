bb0bfe97012c7fefa4c9b4579fd01bef41a8cf2f
Juaanma <juanmanuelsuarez10@gmail.com>
2020-09-28 19:55:01 -0300

    LexicalAnalyzer: shorter error messages

--- src/com/minijava/compiler/lexicalanalyzer/exceptions/UnclosedCommentException.java
+++ src/com/minijava/compiler/lexicalanalyzer/exceptions/UnclosedCommentException.java
@@ -1,10 +1,9 @@
 package com.minijava.compiler.lexicalanalyzer.exceptions;
 
 public class UnclosedCommentException extends LexicalException {
-    private static final String ERROR_MESSAGE = "comentario sin cerrar %s";
+    private static final String ERROR_MESSAGE = "comentario sin cerrar, comenzando con %s";
 
     public UnclosedCommentException(int lineNumber, String lexeme, String line, int lexemePosition) {
-        super(lineNumber, String.format(ERROR_MESSAGE, lexeme), lexeme, line, lexemePosition);
-
+        super(lineNumber, String.format(ERROR_MESSAGE, lexeme.substring(0, lexeme.indexOf('\n'))), lexeme, line, lexemePosition);
     }
 }
--- src/com/minijava/compiler/lexicalanalyzer/exceptions/UnclosedTextBlockException.java
+++ src/com/minijava/compiler/lexicalanalyzer/exceptions/UnclosedTextBlockException.java
@@ -1,9 +1,9 @@
 package com.minijava.compiler.lexicalanalyzer.exceptions;
 
 public class UnclosedTextBlockException extends LexicalException {
-    private static final String ERROR_MESSAGE = "el bloque de texto %s no fue cerrado correctamente.";
+    private static final String ERROR_MESSAGE = "el bloque de texto comenzado por %s no fue cerrado correctamente.";
 
     public UnclosedTextBlockException(int lineNumber, String lexeme, String line, int lexemePosition) {
-        super(lineNumber, String.format(ERROR_MESSAGE, lexeme), lexeme, line, lexemePosition);
+        super(lineNumber, String.format(ERROR_MESSAGE, lexeme.substring(0, lexeme.indexOf('\n'))), lexeme, line, lexemePosition);
     }
 }
