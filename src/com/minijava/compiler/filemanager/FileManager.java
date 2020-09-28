88ae66ef1e0522c3f45e9b46b06582f3a07eb2e0
Juaanma <juanmanuelsuarez10@gmail.com>
2020-09-28 19:39:48 -0300

    FileManager: fixed line numbers

--- src/com/minijava/compiler/filemanager/FileManager.java
+++ src/com/minijava/compiler/filemanager/FileManager.java
@@ -4,19 +4,21 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 public class FileManager implements AutoCloseable {
-    private LineNumberReader reader;
+    private BufferedReader reader;
     private String line = "";
+    private int lineNumber = -1;
     private int charPosition = 0;
 
     public FileManager(String path) throws FileNotFoundException {
         File file = new File(path);
         InputStream inputStream = new FileInputStream(file);
         InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-        reader = new LineNumberReader(inputStreamReader);
+        reader = new BufferedReader(inputStreamReader);
     }
 
     private void readLine() throws IOException {
         line = reader.readLine();
+        lineNumber++;
         charPosition = -1;
     }
 
@@ -37,7 +39,7 @@ public class FileManager implements AutoCloseable {
     }
 
     public int getLineNumber() {
-        return reader.getLineNumber() - 1;
+        return lineNumber;
     }
 
     public int getCharPosition() {
