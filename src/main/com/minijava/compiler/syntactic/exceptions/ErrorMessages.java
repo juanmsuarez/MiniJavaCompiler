35483ff2779bbe5cad6e57870f1dfdfc023b6885
Juaanma <juanmanuelsuarez10@gmail.com>
2020-10-20 10:30:01 -0300

    SyntacticAnalyzer: added error messages

--- src/main/com/minijava/compiler/syntactic/exceptions/ErrorMessages.java
+++ src/main/com/minijava/compiler/syntactic/exceptions/ErrorMessages.java
@@ -4,10 +4,34 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.minijava.compiler.lexical.models.TokenNames.*;
+import static com.minijava.compiler.syntactic.analyzer.TokenGroups.*;
+
 class ErrorMessages {
-    static final Map<String, String> ERROR_MESSAGES = Collections.unmodifiableMap(
+    static final Map<String, String> TOKEN_DESCRIPTION = Collections.unmodifiableMap(
             new HashMap<String, String>() {{
-                // TODO: pendientes mensajes para cada token / token groups
-                // put(EXPECTED_MEMBER_FIRST, "la declaración de ")
-            }});
+                put(EOF, "el fin de archivo");
+                put(CLASS_OR_INTERFACE, "el comienzo de una clase o interfaz");
+                put(TYPE, "un tipo");
+                put(METHOD_TYPE, "un tipo de método");
+                put(ASSIGNMENT_OR_SENTENCE_END, "una asignación o un finalizador de sentencia");
+                put(EXPRESSION, "una expresión");
+                put(OPERAND, "un operando");
+                put(SEMICOLON, "un punto y coma");
+                put(CLASS_ID, "un identificador de clase");
+                put(VAR_MET_ID, "un identificador de método o variable");
+                put(OPEN_BRACE, "una llave de apertura");
+                put(CLOSE_BRACE, "una llave de cierre");
+                put(OPEN_PARENTHESIS, "un paréntesis de apertura");
+                put(CLOSE_PARENTHESIS, "un paréntesis de cierre");
+                put(DOT, "un punto para realizar un acceso");
+                put(GREATER, "un cierre de tipo genérico");
+            }}
+    );
+
+    static final Map<String, String> LEXEME_DESCRIPTION = Collections.unmodifiableMap(
+            new HashMap<String, String>() {{
+                put(EOF, "el fin de archivo");
+            }}
+    );
 }
--- src/main/com/minijava/compiler/syntactic/exceptions/SyntacticException.java
+++ src/main/com/minijava/compiler/syntactic/exceptions/SyntacticException.java
@@ -5,10 +5,16 @@ import com.minijava.compiler.lexical.models.Token;
 
 public class SyntacticException extends CompilerException {
     private static final String ERROR_TYPE = "sintáctico";
-    private static final String ERROR_MESSAGE = "se esperaba %s pero se encontró \"%s\"";
+    private static final String ERROR_MESSAGE = "se esperaba %s pero se encontró %s";
 
     public SyntacticException(Token foundToken, String expectedTokenName, String line, int lexemePosition) {
-        super(ERROR_TYPE, foundToken.getLineNumber(), String.format(ERROR_MESSAGE, expectedTokenName, foundToken.getLexeme()),
+        super(ERROR_TYPE, foundToken.getLineNumber(), buildErrorMessage(expectedTokenName, foundToken),
                 foundToken.getLexeme(), line, lexemePosition);
+    }
+
+    private static String buildErrorMessage(String expectedTokenName, Token foundToken) {
+        return String.format(ERROR_MESSAGE,
+                ErrorMessages.TOKEN_DESCRIPTION.getOrDefault(expectedTokenName, expectedTokenName),
+                ErrorMessages.LEXEME_DESCRIPTION.getOrDefault(foundToken.getName(), "\"" + foundToken.getLexeme() + "\""));
     }
 }
